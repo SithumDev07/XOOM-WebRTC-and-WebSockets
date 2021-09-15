0e13ae718aac882b9dbe447d2a7c35a202ce38da
SithumDev07 <sithum.dashantha@gmail.com>
2021-09-15 20:52:40 +0530

    created express app

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log("Listening on localhost");
+
+app.listen(3000, handleListen);
