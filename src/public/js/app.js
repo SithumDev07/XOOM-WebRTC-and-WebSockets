3bb46a67f30410feb887dc41f02a0b94fca46925
SithumDev07 <sithum.dashantha@gmail.com>
2021-09-16 13:11:41 +0530

    Sending data to the server

---
+++ src/public/js/app.js
@@ -0,0 +1,25 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.binaryType = "usvstring";
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New Message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server");
+});
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value.toString());
+  input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -8,12 +8,21 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("home"));
+app.get("/", (_, res) => res.render("home"));
+// app.get("/*", (_, res) => res.redirect("home"));
 
 const handleListen = () => console.log("Listening on localhost");
 
 const server = http.createServer(app);
 const webss = new WebSocket.Server({ server });
+
+webss.on("connection", (socket) => {
+  console.log("Connected to browser");
+  socket.on("close", () => console.log("Disconnected from the client"));
+  socket.on("message", function incoming(message) {
+    console.log(message);
+  });
+  socket.send("Hello");
+});
 
 server.listen(3000, handleListen);
